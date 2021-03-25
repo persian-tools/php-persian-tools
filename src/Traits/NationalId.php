60e63b1b9a8814e279a028d44f13427c7357736b
farid froozan <froozan@yahoo.com>
2021-03-25 22:43:59 +0430

    remove link from source code

--- src/Traits/NationalId.php
+++ src/Traits/NationalId.php
@@ -23,7 +23,6 @@ trait NationalId
         $zeroFill = 10 - $codeLength;
         $code = str_repeat('0', $zeroFill) . $code;
 
-        // https://www.yjc.ir/fa/news/6787095/%D8%B1%D9%86%D8%AF%D8%AA%D8%B1%DB%8C%D9%86-%D8%B4%D9%85%D8%A7%D8%B1%D9%87-%D9%85%D9%84%DB%8C-%DA%A9%D8%B4%D9%88%D8%B1-%D8%B1%D8%A7-%D8%A8%D8%B4%D9%86%D8%A7%D8%B3%DB%8C%D8%AF
         if($code == '0000000000')
             return false;
 
